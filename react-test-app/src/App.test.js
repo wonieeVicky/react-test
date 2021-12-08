b8938f13ff882fdd10ac29740b7930561ef72e5f
Vicky <hwfongfing@gmail.com>
2021-12-08 09:07:46 +0900

    feat: add initial counter test code

--- react-test-app/src/App.test.js
+++ react-test-app/src/App.test.js
@@ -2,7 +2,10 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  // App 컴포넌트 렌더링
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용해 원하는 엘리먼트에 접근(접근할 때는 ID로 접근한다.)
+  const counterElement = screen.getByTestId('counter');
+  // id가 counter인 엘리먼트의 텍스트가 0인지 테스트한다.
+  expect(counterElement).toHaveTextContent(0);
 });
