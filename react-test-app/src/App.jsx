6ae033a053b9b28113ebdc7347e881cacd724c8d
Vicky <hwfongfing@gmail.com>
2021-12-08 09:08:13 +0900

    feat: test code에 따른 실제 initial counter 구현

---
+++ react-test-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3 data-testid="counter">{counter}</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
