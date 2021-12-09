f887726e97353be51fa5a80deab0d6b5878185b0
Vicky <hwfongfing@gmail.com>
2021-12-09 10:20:45 +0900

    feat: plus, minus 버튼에 대한 테스트 코드 작성

--- react-test-app/src/App.test.js
+++ react-test-app/src/App.test.js
@@ -9,3 +9,15 @@ test('renders learn react link', () => {
   // id가 counter인 엘리먼트의 텍스트가 0인지 테스트한다.
   expect(counterElement).toHaveTextContent(0);
 });
+
+test('minus button has correct text', () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId('minus-button');
+  expect(minusButtonElement).toHaveTextContent('-');
+});
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId('plus-button');
+  expect(plusButtonElement).toHaveTextContent('+');
+});
