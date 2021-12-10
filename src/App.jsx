76ec3bb4255a7c4bf7ccf9b584e21fc2e6323209
Vicky <hwfongfing@gmail.com>
2021-12-10 10:54:39 +0900

    feat: fireEvent 테스트 코드에 대한 실제 Dom 생성

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,12 @@ function App() {
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
         <div>
-          <button data-testid="minus-button">-</button>
-          <button data-testid="plus-button">+</button>
+          <button data-testid="minus-button" onClick={() => setCounter((count) => count - 1)}>
+            -
+          </button>
+          <button data-testid="plus-button" onClick={() => setCounter((count) => count + 1)}>
+            +
+          </button>
         </div>
       </header>
     </div>
