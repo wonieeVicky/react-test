6047eca0e1cf529b700bf01be8bc78ff8aeae05c
Vicky <hwfongfing@gmail.com>
2021-12-10 10:55:16 +0900

    feat: on/off 버튼 생성 테스트 구현(toHaveStyle)

--- src/App.test.js
+++ src/App.test.js
@@ -45,3 +45,9 @@ test('when the - button is pressed, the counter changes to 1', () => {
   const counterElement = screen.getByTestId('counter');
   expect(counterElement).toHaveTextContent(-1);
 });
+
+test('on/off button has blue color', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('on/off-button');
+  expect(buttonElement).toHaveStyle({ backgroundColor: 'blue' });
+});
