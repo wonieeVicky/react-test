8822d0f3f295d1f1de0f81b1bf2070bb05217b60
Vicky <hwfongfing@gmail.com>
2021-12-12 23:29:41 +0900

    feat: toBeDisabled 테스트에 대한 App.js 내 버튼 이벤트 반영

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,26 @@ import './App.css';
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{counter}</h3>
         <div>
-          <button data-testid="minus-button" onClick={() => setCounter((count) => count - 1)}>
+          <button data-testid="minus-button" disabled={disabled} onClick={() => setCounter((count) => count - 1)}>
             -
           </button>
-          <button data-testid="plus-button" onClick={() => setCounter((count) => count + 1)}>
+          <button data-testid="plus-button" disabled={disabled} onClick={() => setCounter((count) => count + 1)}>
             +
           </button>
         </div>
         <div>
-          <button data-testid="on/off-button" style={{ backgroundColor: 'blue' }}>
+          <button
+            data-testid="on/off-button"
+            style={{ backgroundColor: 'blue' }}
+            onClick={() => setDisabled((prev) => !prev)}
+          >
             on/off
           </button>
         </div>
