1d0a19c237178096676f669da4ac2fd9585651c3
Vicky <hwfongfing@gmail.com>
2021-12-18 23:45:29 +0900

    feat: msw를 이용한 상품이미지 가져오기 실제 코드 구현

--- src/2-react-shop-test/pages/OrderPage/Type.js
+++ src/2-react-shop-test/pages/OrderPage/Type.js
@@ -1,7 +1,28 @@
-﻿import React from 'react';
+﻿import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Products from './Products';
 
-const Type = () => {
-  return <div></div>;
-};
+export default function Type({ orderType }) {
+  const [items, setItems] = useState([]);
 
-export default Type;
+  useEffect(() => {
+    loadItems(orderType);
+  }, [orderType]);
+
+  const loadItems = async (orderType) => {
+    try {
+      let response = await axios.get(`http://localhost:5000/${orderType}`);
+      setItems(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const ItemComponent = orderType === 'products' ? Products : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
+  ));
+
+  return <div>{optionItems}</div>;
+}
