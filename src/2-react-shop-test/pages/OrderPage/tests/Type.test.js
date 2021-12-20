0f906742e1a58b63a7ef0c1221ae7947f26dd3af
Vicky <hwfongfing@gmail.com>
2021-12-20 13:38:18 +0900

    feat: server data-fetching error 시 ErrorBanner 처리 테스트 코드 구현

--- src/2-react-shop-test/pages/OrderPage/tests/Type.test.js
+++ src/2-react-shop-test/pages/OrderPage/tests/Type.test.js
@@ -1,4 +1,6 @@
 ﻿import { screen, render } from '@testing-library/react';
+import { rest } from 'msw';
+import { server } from '../../../mocks/server';
 import Type from '../Type';
 
 test('displays product images from server', async () => {
@@ -12,4 +14,13 @@ test('displays product images from server', async () => {
 
   const altText = productImages.map((element) => element.alt);
   expect(altText).toEqual(['America product', 'England product']);
+});
+
+test('when fetching product datas, face an error', async () => {
+  server.resetHandlers(rest.get('http://localhost:5000/products', (req, res, ctx) => res(ctx.status(500))));
+
+  render(<Type orderType="products" />);
+
+  const errorBanner = await screen.findByTestId('error-banner');
+  expect(errorBanner).toHaveTextContent('에러가 발생했습니다.');
 });
