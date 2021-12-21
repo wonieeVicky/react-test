d9a0f96dabb2dac58dd222c34d9ddef1c89610f3
Vicky <hwfongfing@gmail.com>
2021-12-21 23:52:15 +0900

    feat: Options 컴포넌트 생성

---
+++ src/2-react-shop-test/pages/OrderPage/Options.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+
+const Options = ({ name }) => {
+  return (
+    <form>
+      <input type="checkbox" id={`${name} option`} />
+      {'  '}
+      <label htmlFor={`${name} option`}>{name}</label>
+    </form>
+  );
+};
+
+export default Options;
