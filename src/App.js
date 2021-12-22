15d75a428111d6f0c994f6d942151cddf8c2ab47
Vicky <hwfongfing@gmail.com>
2021-12-22 23:27:08 +0900

    feat: ui 확인을 위해 App.js에 OrderPage 반영

---
+++ src/App.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import OrderPage from './2-react-shop-test/pages/OrderPage/OrderPage';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ padding: '4rem' }}>
+      <OrderPage />
+    </div>
+  );
+}
+
+export default App;
