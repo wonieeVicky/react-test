7c0311988b3b66090fb55883b77011ea331c498c
Vicky <hwfongfing@gmail.com>
2021-12-28 23:45:53 +0900

    feat: 상품 + 옵션 총 합계 산출 로직 테스트 코드 구현

--- src/2-react-shop-test/pages/OrderPage/tests/calculate.test.js
+++ src/2-react-shop-test/pages/OrderPage/tests/calculate.test.js
@@ -1,6 +1,7 @@
 ﻿import { render, screen } from '../../../test-utils';
 import userEvent from '@testing-library/user-event';
 import Type from '../Type';
+import OrderPage from '../OrderPage';
 
 test("update product's total when products change", async () => {
   render(<Type orderType="products" />);
@@ -48,4 +49,47 @@ test("update option's total when options change", async () => {
   // 디너 옵션 제거
   userEvent.click(dinnerCheckbox);
   expect(optionsTotal).toHaveTextContent('500');
+});
+
+describe('total price of goods and options', () => {
+  test('total price starts with 0 and Updating total price when adding one product', async () => {
+    render(<OrderPage />);
+
+    // const total = screen.getByRole('heading:', { name: /Total Price: i$/ });
+    const total = screen.getByText('Total Price:', { exact: false });
+    expect(total).toHaveTextContent('0');
+
+    const americaInput = await screen.findByRole('spinbutton', { name: 'America' });
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, '1');
+    expect(total).toHaveTextContent('1000');
+  });
+  test('Updating total price when adding one option', async () => {
+    render(<OrderPage />);
+
+    const total = screen.getByText('Total Price:', { exact: false });
+    const insuranceCheckbox = await screen.findByRole('checkbox', {
+      name: 'Insurance',
+    });
+    userEvent.click(insuranceCheckbox);
+    expect(total).toHaveTextContent('500');
+  });
+  test('Updating total price when removing option and product', async () => {
+    render(<OrderPage />);
+
+    const total = screen.getByText('Total Price:', { exact: false });
+    const insuranceCheckbox = await screen.findByRole('checkbox', {
+      name: 'Insurance',
+    });
+    userEvent.click(insuranceCheckbox);
+
+    const americaInput = await screen.findByRole('spinbutton', { name: 'America' });
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, '3');
+
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, '1');
+
+    expect(total).toHaveTextContent('1500');
+  });
 });
