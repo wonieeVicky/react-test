28c940e3a9de32a21e5ef080c11bb4aed588c8cc
Vicky <hwfongfing@gmail.com>
2021-12-30 23:57:35 +0900

    feat: 주문 확인 페이지 로직 tdd 구현

--- src/App.test.js
+++ src/App.test.js
@@ -12,7 +12,7 @@ test('From order to order completion', async () => {
   userEvent.type(americaInput, '2');
 
   // England 여행 상품 3개 추가
-  const englandInput = screen.getByRole('spinbutton', { name: 'England' });
+  const englandInput = await screen.findByRole('spinbutton', { name: 'England' });
   userEvent.clear(englandInput);
   userEvent.type(englandInput, '3');
 
@@ -25,4 +25,31 @@ test('From order to order completion', async () => {
     name: '주문하기',
   });
   userEvent.click(orderButton);
+
+  // 주문 확인 페이지
+  // 제목
+  const summaryHeading = screen.getByRole('heading', { name: '주문 확인' });
+  expect(summaryHeading).toBeInTheDocument();
+
+  // 여행 상품 총 가격
+  const productsHeading = screen.getByRole('heading', { name: '여행 상품: 5000' });
+  expect(productsHeading).toBeInTheDocument();
+
+  // 옵션 총 가격
+  const optionsHeading = screen.getByRole('heading', { name: '옵션: 500' });
+  expect(optionsHeading).toBeInTheDocument();
+
+  // 특정 상품 나열
+  expect(screen.getByText('2 America')).toBeInTheDocument();
+  expect(screen.getByText('3 England')).toBeInTheDocument();
+  expect(screen.getByText('Insurance')).toBeInTheDocument();
+
+  // 체크 박스 체크
+  const confirmCheckbox = screen.getByRole('checkbox', { name: '주문하려는 것을 확인하셨나요?' });
+  userEvent.click(confirmCheckbox);
+
+  const confirmOrderButton = screen.getByRole('button', {
+    name: '주문 확인',
+  });
+  userEvent.click(confirmOrderButton);
 });
