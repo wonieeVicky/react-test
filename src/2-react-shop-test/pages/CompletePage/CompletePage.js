7a56d1f5995d5b94f013edfb8035f994af429186
Vicky <hwfongfing@gmail.com>
2022-01-03 00:57:33 +0900

    feat: 주문 완료 페이지 테스트 코드 구현에 따른 실제 코드 구현

--- src/2-react-shop-test/pages/CompletePage/CompletePage.js
+++ src/2-react-shop-test/pages/CompletePage/CompletePage.js
@@ -1,7 +1,60 @@
-﻿import React from 'react';
+﻿import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+import { OrderContext } from '../../contexts/OrderContext';
+import ErrorBanner from '../../components/ErrorBanner';
 
-function CompletePage() {
-  return <div></div>;
+function CompletePage({ setStep }) {
+  const [orderDatas] = useContext(OrderContext);
+  const [orderHistory, setOrderHistory] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => orderCompleted(orderDatas), [orderDatas]);
+
+  const orderCompleted = async (orderDatas) => {
+    try {
+      let response = await axios.post(`http://localhost:5000/order`, orderDatas);
+      setOrderHistory(response.data); // [{ orderNumber: 123455676, price: 2000 }]
+      setLoading(false);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  if (error) {
+    return <ErrorBanner message="에러가 발생했습니다." />;
+  }
+
+  const orderTable = orderHistory.map((item) => (
+    <tr key={item.orderNumber}>
+      <td>{item.orderNumber}</td>
+      <td>{item.price}</td>
+    </tr>
+  ));
+
+  if (loading) {
+    return <div>loading</div>;
+  } else {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <h2>주문이 성공했습니다.</h2>
+        <h3>지금까지 모든 주문</h3>
+        <table style={{ margin: 'auto' }}>
+          <tbody>
+            <tr>
+              <th>주문 번호</th>
+              <th>주문 가격</th>
+            </tr>
+            {orderTable}
+          </tbody>
+        </table>
+        <br />
+        <button className="rainbow rainbow-1" onClick={() => setStep(0)}>
+          첫페이지로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default CompletePage;
